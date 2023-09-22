17f0bb4ed0edcebea62fe3725e2f1fef4cea4f67
ddong <65761809+ddongcui@users.noreply.github.com>
2023-09-22 16:01:16 +0800

    Update check_hw.py
    check the height&width of images

--- check_hw.py
+++ check_hw.py
@@ -2,21 +2,18 @@ import os
 from PIL import Image
 from tqdm import tqdm
 
-# 图像文件夹路径
+# images path
 folder_path = 'images'
 
-# 遍历图像文件夹中的所有文件
+# get all images
 for filename in tqdm(os.listdir(folder_path)):
-    # 获取图像文件的完整路径
+    # get full path of image
     file_path = os.path.join(folder_path, filename)
     
-    # 读取图像
+    # open image
     image = Image.open(file_path)
     
-    # 获取图像的宽度和高度
+    # get the height&width of image
     width, height = image.size
-    
-    # 判断宽度和高度是否等于1920和1080
     if width != 1920 or height != 1080:
-        # 输出图像文件名
         print(filename)
